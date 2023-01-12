166c33a232441b74c3109eece79ea96c07919939
Paul N Barnes <paulieb99@gmail.com>
2023-01-11 23:12:35 -0800

    created Ghost Class file, added the constructor with variables. imported ghost file to index.js

---
+++ Ghost.js
@@ -0,0 +1,15 @@
+import { DIRECTIONS, OBJECT_TYPE } from './setup';
+
+class Ghost {
+  constructor(speed = 5, startPos, movement, name) {
+    this.name = name;
+    this.movement = movement;
+    this.startPos = startPos;
+    this.pos = startPos;
+    this.dir = DIRECTIONS.ArrowRight;
+    this.speed = speed;
+    this.timer = 0;
+    this.isScared = false;
+    this.rotation = false;
+  }
+}
