884a0746219257d43ace71e60aa31bf11c845169
Paul N Barnes <paulieb99@gmail.com>
2023-01-12 21:11:01 -0800

    created while loop to randomly calculate the key for the directions array, it also acts as a check for the walls and other ghosts.

--- ghostMove.js
+++ ghostMove.js
@@ -7,4 +7,14 @@ export function randmomMovement(position, direction, objectExist) {
   let nextMovePos = position + dir.movement;
   // Create an array from the directions object keys
   const keys = object.keys(DIRECTIONS);
+
+  while (
+    objectExist(nextMovePos, OBJECT_TYPE.WALL) ||
+    objectExist(nextMovePos, OBJECT_TYPE.GHOST)
+  ) {
+    const key = keys[Math.floor(Math.random() * keys.length)];
+
+    // set the next move
+    dir = DIRECTIONS[key];
+  }
 }
