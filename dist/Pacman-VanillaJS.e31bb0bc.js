86e20421783ae1f5ce05e5905761cde036926eac
Paul N Barnes <paulieb99@gmail.com>
2023-01-15 15:31:46 -0800

    ran npm run build command

--- dist/Pacman-VanillaJS.e31bb0bc.js
+++ dist/Pacman-VanillaJS.e31bb0bc.js
@@ -777,7 +777,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59759" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63195" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
